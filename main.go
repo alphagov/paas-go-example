5e4ff499278b77eee6e2b667026cb69ed6676271
Chris Patuzzo <chris@patuzzo.co.uk>
2018-04-03 16:50:14 +0100

    Read a list of countries from a JSON file

--- main.go
+++ main.go
@@ -1,26 +1,55 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
-  "os"
+    "encoding/json"
+    "io/ioutil"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
 )
 
-func handlerRoot(w http.ResponseWriter, r *http.Request) {
-  queries := r.URL.Query()
-  name := queries.Get("name")
+type Record struct {
+    Item []Country `json:"item"`
+}
 
-  if len(queries.Get("name")) > 0 {
-    fmt.Fprintf(w, "Hello, %s", name)
-  } else {
-    fmt.Fprintf(w, "name cannot be blank")
-  }
+type Country struct {
+    Name string `json:"name"`
+}
+
+func init() {
+    countries := readCountries()
+    fmt.Printf(">>>>> %s\n", countries)
+}
+
+func readCountries() []Country {
+    raw, _ := ioutil.ReadFile("./countries.json")
+
+    var records map[string]Record
+    json.Unmarshal(raw, &records)
+
+    var countries []Country
+    for _, record := range records {
+        countries = append(countries, record.Item[0])
+    }
+
+    return countries;
+}
+
+func handlerRoot(w http.ResponseWriter, r *http.Request) {
+    queries := r.URL.Query()
+    name := queries.Get("name")
+
+    if len(queries.Get("name")) > 0 {
+        fmt.Fprintf(w, "Hello, %s", name)
+    } else {
+        fmt.Fprintf(w, "name cannot be blank")
+    }
 }
 
 func main() {
-  port := os.Getenv("PORT")
+    port := os.Getenv("PORT")
 
-  http.HandleFunc("/", handlerRoot)
-  log.Fatal(http.ListenAndServe(":" + port, nil))
+    http.HandleFunc("/", handlerRoot)
+    log.Fatal(http.ListenAndServe(":" + port, nil))
 }
